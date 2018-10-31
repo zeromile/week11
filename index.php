9c9dcff430b52acffe6f720d7099c665ba97317c
zeromile <jasonleecooksey@gmail.com>
2018-10-31 07:09:33 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "zeromile";
+$password = "";
+$dbname = "c9";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
+
+//echo "Connected successfully, yo";
+
+// Get table records
+$sql = "SELECT * FROM people";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    // output data of each row
+    while($row = $result->fetch_assoc()){
+        echo $row["first_name"]." ".$row["last_name"].", favorite color: ".$row["fav_color"]."<br/>";
+    }
+} else {
+    echo "No results";
+}
+
+$conn->close();
+
+?>
